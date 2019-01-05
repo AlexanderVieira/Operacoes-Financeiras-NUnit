ca4d04bf1ca3f9b201c8400cc12f7ee3b1e96013
alexandervieira <alexander.silva@al.infnet.edu.br>
2019-01-04 22:09:46 -0200

    Inicial Commit

---
+++ CalculadoraFinanceira/CalculadoraFinanceira.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperacaoFinanceira 
+{
+	public static class CalculadoraFinanceira
+	{
+		/// <summary>
+		/// Método que calcula juros composto
+		/// @author Alexander
+		/// </summary>
+		/// <param name="capital"></param>
+		/// <param name="periodo"></param>
+		/// <param name="taxa"></param>
+		/// <returns>Retorna um valor do tipo double</returns>
+		public static double CalcularJurosComposto(
+			double capital, int periodo, double taxa)
+		{
+			return Math.Round(capital * Math.Pow((1 + taxa / 100), periodo), 2); ;
+		}
+    }
+}
---
+++ NUnit.Testes/TesteCalculaJurosComposto.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OperacaoFinanceira;
+
+namespace NUnit.Testes
+{
+
+	/// <summary>
+	/// Classe de teste criada para garantir o funcionamento
+	///	das principais operações realizadas.
+	///	@author Alexander	
+	/// </summary>
+	[TestFixture]
+	public class TesteCalculadoraFinanceira
+	{
+		[TestCase(10000, 12, 2, 12682.42)]
+		[TestCase(15000, 36, 6, 122208.78)]
+		[TestCase(12000, 48, 3, 49587.02)]
+		public void TesteCalcularJurosComposto(
+			double capital, int periodo, double taxa, double valorEsperado)
+		{
+			/* ======================== Execução ===================== */
+			var montante = CalculadoraFinanceira.CalcularJurosComposto(
+				capital, periodo, taxa);
+
+			/* ====================== Verificação ==================== */
+			Assert.AreEqual(valorEsperado, montante);
+		}
+	}
+}
